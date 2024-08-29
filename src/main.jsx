a67fae0ea217d1e42a1d20166a2215a42fb16ad0
Rubenhdz20 <rubhernandez42@gmail.com>
2024-08-28 18:46:41 -0600

    Remove tailwind and use CSS Modules instead

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './input.css'
+import './app.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
